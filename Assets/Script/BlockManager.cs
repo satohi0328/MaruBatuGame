bd39b41f692cf5157c303e47e68d67d99ea65e28
satohi0328 <31176803+satohi0328@users.noreply.github.com>
2020-04-15 20:44:42 +0900

    Fix
    盤面をタップした際に、オブジェクトが生成されるように修正

---
+++ Assets/Script/BlockManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+    }
+    // Update is called once per frame
+    void Update() {
+
+    }
+    // 剣(○)Prefab
+    public GameObject BastardSwordPrefab;
+    // 盾(×)Prefab
+    public GameObject KnightlyShieldPrefab;
+
+
+    private int posX;       //x座標
+    private int posY;       //y座標
+    private string status;  //○、×、カラの状態を保持
+    private GameObject objSoard; //ゲームオブジェクト maru
+    private GameObject objShild; //ゲームオブジェクト batu
+
+    /**
+     * 初期化処理
+     */
+    public void init(int x, int y) {
+        this.posX = x;
+        this.posY = y;
+        this.status = null;
+
+        //オブジェクト生成し、非表示
+        // 剣(○)オブジェクト生成
+        this.objSoard = Instantiate(BastardSwordPrefab) as GameObject;
+        //位置を移動
+        Vector3 soardPos = this.transform.position;
+        soardPos.y = 7f;
+        this.objSoard.transform.position = soardPos;
+        this.objSoard.SetActive(false);
+
+        // 盾(×)オブジェクト生成
+        this.objShild = Instantiate(KnightlyShieldPrefab) as GameObject;
+        //位置を移動
+        Vector3 shildPos = this.transform.position;
+        shildPos.y = 9f;
+        this.objShild.transform.position = shildPos;
+        this.objShild.SetActive(false);
+    }
+
+
+    /**
+    * マス上にオブジェクトを表示できるか判定
+    */
+    public bool judgeShowObject() {
+
+        // 一度も表示されていない場合
+        if (this.status == null) {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+    * マス上にオブジェクトを表示
+    * 引数:true(○),false(×)
+    */
+    public void showObject(bool arg) {
+        if (arg) {
+            this.objSoard.SetActive(true);
+            this.status = "maru";
+        } else {
+            this.objShild.SetActive(true);
+            this.status = "batu";
+        }
+    }
+
+}
--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -5,60 +5,66 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
     // 剣(○)Prefab
     public GameObject BastardSwordPrefab;
-    // 盾(○)Prefab
+    // 盾(×)Prefab
     public GameObject KnightlyShieldPrefab;
+    // BlockPrefab
+    public GameObject BlockPrefab;
 
-    public GameManager[,] boardStatus = new GameManager[2, 2];
+
+    // まるばつの設置回数を保持
+    private int setCounter = 0;
+
+
+    public BlockManager[,] boardStatus = new BlockManager[3, 3];
     // Use this for initialization
     void Start() {
 
-        // 盤面3×3の各マスにGameManagerを生成
+        // 盤面3×3の各マスにBlockManagerを生成
         for (int i = 0; i < boardStatus.GetLength(0); i++) {
             for (int j = 0; j < boardStatus.GetLength(1); j++) {
+                // 盤面配列に格納するオブジェクト生成
+                GameObject wkObj = Instantiate(BlockPrefab) as GameObject;
+                // 座標設定
+                wkObj.transform.position = new Vector3(j * 20 - 20, 2, i * 20 - 20);
 
+                // BlockManagerを取得
+                BlockManager gameManager = wkObj.GetComponent<BlockManager>();
+                boardStatus[i, j] = gameManager;
+                // マスごとに初期化
                 boardStatus[i, j].init(i, j);
-
-
-                GameObject obj = Instantiate(BastardSwordPrefab) as GameObject;
-
             }
-
         }
+
+
+
     }
 
     // Update is called once per frame
     void Update() {
 
-    }
+        // 左クリックされた場合
+        if (Input.GetMouseButtonDown(0)) {
+            //カメラからの光線を設定
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            // 光線がオブジェクトに衝突した場合
+            if (Physics.Raycast(ray, out hit, 100.0f)) {
+                BlockManager gm = hit.collider.GetComponent<BlockManager>();
 
-}
-
-
-public class GameManager : MonoBehaviour {
-    private int posX;       //x座標
-    private int posY;       //y座標
-    private string status;  //○、×、カラの状態を保持
-    private GameObject obj; //ゲームオブジェクト
-
-    /**
-     * 初期化処理
-     */
-    public void init(int x, int y) {
-        this.posX = x;
-        this.posY = y;
-        this.status = "";
-        this.obj = null;
-    }
-
-    /**
-     * 盤面上にオブジェクトを生成
-     */
-    public void generateObjectOnBoard(GameObject argObj) {
-        this.obj = argObj;
-        this.obj.transform.position = new Vector3(this.posX * 20 - 20, 10, this.posY * 20 - 20);
-
+                //オブジェクトの表示判定
+                if (gm.judgeShowObject()) {
+                    // 偶数回目の場合は剣(○)
+                    if (setCounter % 2 == 0) {
+                        gm.showObject(true);
+                    } else {
+                        gm.showObject(false);
+                    }
+                    setCounter++;
+                }
+                Debug.Log(hit.collider.gameObject.transform.position);
+            }
+        }
 
     }
-
 
 }
